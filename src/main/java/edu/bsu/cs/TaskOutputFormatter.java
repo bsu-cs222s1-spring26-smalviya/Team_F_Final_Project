419af9e19bed16b7a73823c1438d1be77fe0166e
KyrionRan <kyrionran@outlook.com>
2026-03-18 17:05:38 -0400

    Add TaskOutputFormatter Class

---
+++ src/main/java/edu/bsu/cs/TaskOutputFormatter.java
@@ -0,0 +1,15 @@
+package edu.bsu.cs;
+
+import java.util.List;
+
+public class TaskOutputFormatter {
+    public String getTaskOutput(List<Task> taskList) {
+        StringBuilder output = new StringBuilder();
+        output.append("Task\n" +
+                "Timestamp\tTitle\tContent\n");
+        for (Task i : taskList) {
+            output.append(i);
+        }
+        return output.toString();
+    }
+}
