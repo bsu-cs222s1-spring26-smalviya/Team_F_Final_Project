2620cbb1d83906b70154f934aa5e50e85c466a1f
KyrionRan <kyrionran@outlook.com>
2026-03-18 15:32:07 -0400

    Refactor: Align Task with project requirements

--- src/test/java/edu/bsu/cs/TaskTest.java
+++ src/test/java/edu/bsu/cs/TaskTest.java
@@ -4,21 +4,39 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TaskTest {
-    private Task task = new Task("Finish Project", "Complete User Story 4", "Software Engineering");
+    private Task task = new Task("2026-01-25T23:51:24Z", "Complete User Story 4", "Software Engineering");
 
     @Test
-    public void testGetNote() {
-        Assertions.assertEquals("Complete User Story 4", task.getNote());
+    public void testSetTimestamp() {
+        task.setTimestamp("2026-01-26T23:51:24Z");
+        Assertions.assertEquals("2026-01-26T23:51:24Z", task.getTimestamp());
     }
 
     @Test
-    public void testGetCourseName() {
-        Assertions.assertEquals("Software Engineering", task.getCourseName());
+    public void testGetTimestamp() {
+        Assertions.assertEquals("2026-01-25T23:51:24Z", "2026-01-25T23:51:24Z");
     }
 
     @Test
-    public void testGetTimestampIsGenerated() {
-        Assertions.assertNotNull(task.getTimestamp());
-        Assertions.assertFalse(task.getTimestamp().isEmpty());
+    public void testSetTitle() {
+        task.setTitle("Complete User Story 5");
+        Assertions.assertEquals("Complete User Story 5", task.getTitle());
     }
+
+    @Test
+    public void testGetTitle() {
+        Assertions.assertEquals("Complete User Story 4", task.getTitle());
+    }
+
+    @Test
+    public void testSetContent() {
+        task.setContent("Software Developing");
+        Assertions.assertEquals("Software Developing", task.getContent());
+    }
+
+    @Test
+    public void testGetContent() {
+        Assertions.assertEquals("Software Engineering", task.getContent());
+    }
+
 }
