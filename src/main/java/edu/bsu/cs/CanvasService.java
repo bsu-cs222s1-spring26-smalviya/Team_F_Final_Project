fd94f1d1cc6f08469ba7fead3b8c45753d822d03
Ammar Hassan <ammar.hassan@bsu.edu>
2026-03-19 00:04:33 -0400

    Added getAssignmets to CanvasService.java to fetch and return assignments for a given course.

--- src/main/java/edu/bsu/cs/CanvasService.java
+++ src/main/java/edu/bsu/cs/CanvasService.java
@@ -20,4 +20,19 @@ public class CanvasService {
     public List<Course> getCourses() {
         return this.courses;
     }
+
+    public List<Assignment> getAssignments(String courseId) throws IOException, URISyntaxException {
+        String jsonData = new CanvasConnector(user).fetchAssignments(courseId);
+        AssignmentParser parser = new AssignmentParser(jsonData);
+        List<Assignment> assignments = parser.getAssignments();
+
+        // Sort by due date (nulls last)
+        assignments.sort((a, b) -> {
+            if (a.getDueAt().equals("null")) return 1;
+            if (b.getDueAt().equals("null")) return -1;
+            return a.getDueAt().compareTo(b.getDueAt());
+        });
+
+        return assignments;
+    }
 }
