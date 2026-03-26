9e9f39a9f8bc8e6c9d7800197677b17ac6a6506d
Ammar Hassan <ammar.hassan@bsu.edu>
2026-03-26 14:43:59 -0400

    Polished console outputs for Main Menu, Course list, and Assignment list

--- src/main/java/edu/bsu/cs/CanvasOutputFormatter.java
+++ src/main/java/edu/bsu/cs/CanvasOutputFormatter.java
@@ -5,21 +5,47 @@ import java.util.List;
 public class CanvasOutputFormatter {
     public String getCoursesOutput(List<Course> courses) {
         StringBuilder output = new StringBuilder();
-        output.append("Course\n" +
-                "ID\tName\n");
+        System.out.println("**********************************************************************");
+        System.out.println("*                               Courses                              *");
+        System.out.println("**********************************************************************");
+        System.out.println("ID                  |  Course Name");
+        System.out.println("----------------------------------------------------------------------");
         for (Course i : courses) {
-            output.append(i.getId() + "\t" + i.getName() + "\n");
+            output.append(i.getId() + "\t" + "|  " + i.getName() + "\n");
         }
         return output.toString();
     }
 
     public String getAssignmentsOutput(List<Assignment> assignments) {
         StringBuilder output = new StringBuilder();
-        output.append("Assignments\n");
-        output.append("ID\tName\tDue Date\tDescription\n");
+        output.append("**********************************************************************\n");
+        output.append("*                            Assignments                             *\n");
+        output.append("**********************************************************************\n");
+        output.append(String.format("%-20s | %-25s | %s\n", "Name", "Due Date", "Description"));
+        output.append("----------------------------------------------------------------------\n");
         for (Assignment a : assignments) {
-            output.append(a.getId() + "\t" + a.getName() + "\t" + a.getDueAt() + "\t" + a.getDescription() + "\n");
+            String cleanDescription = stripHtml(a.getDescription());
+            String dueDate = (a.getDueAt() == null || a.getDueAt().equals("null"))
+                    ? "No Due Date"
+                    : a.getDueAt();
+            output.append(String.format("%-20s | %-25s | %s\n",
+                    a.getName(), dueDate, cleanDescription));
         }
         return output.toString();
+    }
+
+
+    private String stripHtml(String html) {
+        if (html == null || html.equals("null")) {
+            return "No Description";
+        }
+
+        String noTags = html.replaceAll("<[^>]*>", "");
+        String cleaned = noTags.replaceAll("\\s+", " ").trim();
+        if (cleaned.length() > 80) {
+            return cleaned.substring(0, 77) + "...";
+        }
+
+        return cleaned;
     }
 }
